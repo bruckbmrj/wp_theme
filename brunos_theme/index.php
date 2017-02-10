ad5ec7969b3f5fb5904a8a656cd3f3ea9fec7797
Bruno Braga Mendonça <bruckbmrj@hotmail.com>
2017-02-10 15:29:20 -0200

    estrutura header

--- brunos_theme/index.php
+++ brunos_theme/index.php
@@ -8,6 +8,43 @@
 <?php wp_head(); ?>
 </head>	
 	<body>
+
+	<div id="header">
+		
+		<div id="header-superior">
+			<div id="header-superior-content">
+				<div id="header-paginas">
+					Header Paginas
+				</div>
+
+				<div id="header-social">
+					Header Social
+				</div>
+
+			</div>
+		</div>
+
+		<div id="header-contet">
+			<div id="logo">
+				Logo
+			</div>
+
+			<div id="login">
+				Login
+			</div>
+
+			<div id="search">
+				Search
+			</div>
+		</div>
+
+		<div id="nav">
+			<div id="nav-content">
+				Barra de navegação
+			</div>
+		</div> 
+
+	</div> <!-- Fim header -->
 		
 	<?php wp_footer(); ?>	
 	</body>
